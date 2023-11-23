7c3c2d7c34283f2078f2d9c73a8064addcaa74fd
florishenkelman <florishenkelman@gmail.com>
2023-11-23 10:43:56 +0100

    Nog goede zinnen toegevoegd

--- src/Ingredients.java
+++ src/Ingredients.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Ingredients {
+public class Ingredients {
+    public static void main(String[] args) {
+        System.out.println("Hahahaha");
+        System.out.println("Jahaaaaa");
+    }
 }
